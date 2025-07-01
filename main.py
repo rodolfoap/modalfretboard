4de69787ca705073278e2a2448c247360fc865d5
RodolfoAP <rodolfo.alcazar@gtd.eu>
2025-07-01 19:52:22 +0200

    Changes 2025/07/01-19:52:22

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 #!/bin/env python3
 notnames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 fbsize=22
-def fret(note):
-	return note.center(6, '-')+'|'
 
-def string(offset):
+def string(offset, scale):
 	for i, x in enumerate(range(offset, offset+fbsize+1)):
+		x%=12
 		width=3 if i==0 else 6
-		print((notnames[x%12]).center(width, '-')+'|', end='')
-		#print(fret(, end='')
+		note_in_scale=notnames[x] if x in scale else '='
+		print((note_in_scale).center(width, '-')+'|', end='')
 	print()
 
-def fretboard(scale_intervals):
+def fretboard(scale):
 	# Index line (0, 1, 2, ... 22)
+	print()
 	for x in range(0, fbsize+1):
 		width=3 if x==0 else 7
 		print(str(x).center(width, ' '), end='')
 	print()
-	string(4)
-	string(11)
-	string(7)
-	string(2)
-	string(9)
-	string(4)
+	string(4, scale)
+	string(11, scale)
+	string(7, scale)
+	string(2, scale)
+	string(9, scale)
+	string(4, scale)
+	print()
 
 c_maj = [0, 2, 4, 5, 7, 9, 11]
 fretboard(c_maj)
